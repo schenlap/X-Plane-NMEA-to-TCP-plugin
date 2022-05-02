3f2718940662eb59b0314a38e24b1d46d8ea7b2e
Stefan Schöfegger <memo5@gmx.at>
2022-05-02 19:25:30 +0200

    cleanup and coding style

--- PI_XTCPgps.py
+++ PI_XTCPgps.py
@@ -5,19 +5,18 @@ copyright (c) 2009 Timor at cyhex; released under GPL 2.0 or later
 XTCPgps is a plugin for X-Plane. Based on Ed Park's XMAPgps, It sends these NMEA sentences
 over a TCP connection to mapping software: GPRMC, GPGGA, dummy GPGSA.
 
-Demonstrated to work with XCsoar 6.1 running on a Android 2.2 and 
-with XCsoar running on a Linux desktop.  
+Demonstrated to work with XCsoar 6.1 running on a Android 2.2 and
+with XCsoar running on a Linux desktop.
 
-Alex Ferrer 2014: added LXWP0 NMEA sentence so Xsoar (for use with Xcsoar condor driver) 
+Alex Ferrer 2014: added LXWP0 NMEA sentence so Xsoar (for use with Xcsoar condor driver)
                   can take advantage of vario, IAS and Wind info
-schenlap: modified for python3 X-Plane plugin
+schenlap 2022: modified for python3 X-Plane plugin
 """
 
 from XPLMProcessing import *
 from XPLMDataAccess import *
 from XPLMUtilities import *
 
-#import math
 from datetime import date
 import time
 import sys
@@ -43,14 +42,14 @@ class SocketPlugin(object):
     #Edit here, yout host and port
     HOST = ('192.168.1.45',4353)
     connected = False
-    
+
     def __init__(self):
         self.connect()
-        
-    
+
+
     def connect(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
+
         try:
             self.s.connect(self.HOST)
             self.connected = True
@@ -58,37 +57,30 @@ class SocketPlugin(object):
             OutputFile.write(" Can not connect to NMEA Client on %s:%s %s \n" % (self.HOST[0],self.HOST[1], e))
             OutputFile.flush()
             self.connected = False
-        
-        
-        
-               
-            
-                
+
     def write(self,data):
         if not self.connected:
             self.connect()
-                
         try:
             self.s.send(data)
         except:
             self.connected = False
             self.s.close()
-            
-        
-    
+
+
 class PythonInterface:
         def XPluginStart(self):
             self.Name = "XTCPgps"
             self.Sig =  "Timor.Python.XTCPgps"
             self.Desc = "A plugin to send NMEA sentences to mapping software over TCP."
-    
+
             # For possible debugging use:
             # Open a file to write to, located in the same directory as this plugin.
             self.OutputFile = OutputFile
             self.LineCount = 0
             self.ser = SocketPlugin()
-            
-            
+
+
             # test if self.ser is writable
             test_thread = threading.Thread(target=self.ser.write, args=("HELLO?",))
             before_count = threading.activeCount()
@@ -96,32 +88,31 @@ class PythonInterface:
             time.sleep(0.1)
             after_count = threading.activeCount()
             self.CannotWrite = after_count - before_count
-    
+
             self.OutputFile.write('Thread count before: ' + str(before_count) + ' after: ' + str(after_count) + ' CannotWrite: ' + str(self.CannotWrite) + '\n')
             self.LineCount = self.LineCount + 1
             self.OutputFile.flush()
-    
+
             # Locate data references for all communicated variables.
-    
             # time and date
             self.drZulu_time = XPLMFindDataRef("sim/time/zulu_time_sec")
             self.drDate      = XPLMFindDataRef("sim/time/local_date_days")
             # probably ok to set fixed date from system, not x-plane
             self.n_date = date.today().strftime("%d%m%y")
-    
+
             # ground speed
             self.drVgnd_kts = XPLMFindDataRef("sim/flightmodel/position/groundspeed")
-    
+
             # magnetic heading and variation
             self.drHding_mag = XPLMFindDataRef("sim/flightmodel/position/magpsi")
             self.drMag_var   = XPLMFindDataRef("sim/flightmodel/position/magnetic_variation")
-    
+
             # latitude, longitude, and altitude
             self.drLat_deg = XPLMFindDataRef("sim/flightmodel/position/latitude")
             self.drLon_deg = XPLMFindDataRef("sim/flightmodel/position/longitude")
             self.drAlt_ind = XPLMFindDataRef("sim/flightmodel/position/elevation")
 
-            # indicated airspeed  
+            # indicated airspeed
             self.drIAS_ind = XPLMFindDataRef("sim/flightmodel/position/indicated_airspeed")
 
             # wind vector currently acting on the plane in KTS
@@ -131,7 +122,7 @@ class PythonInterface:
             # barometric pressure
             self.drBaro_alt = XPLMFindDataRef("sim/flightmodel/misc/h_ind")
             self.drVario_fpm  = XPLMFindDataRef("sim/cockpit2/gauges/indicators/total_energy_fpm")
-    
+
             # Register our callback for once per 1-second.  Positive intervals
             # are in seconds, negative are the negative of sim frames.  Zero
             # registers but does not schedule a callback for time.
@@ -154,15 +145,12 @@ class PythonInterface:
             pass
 
         def FlightLoopCallback(self, elapsedMe, elapsedSim, counter, refcon):
-    
             if self.CannotWrite:
-    
                 return 10.0
-    
             #self.where = 1
             #self.OutputFile.write('where?  ' + str(self.where) + '  ' + str(time.clock()) + '\n')
             #self.LineCount = self.LineCount + 1
-    
+
             # Get current values for communicated variables.
             self.Zulu_time = XPLMGetDataf(self.drZulu_time)  # sec since midnight
             self.Lat_deg = XPLMGetDatad(self.drLat_deg)
@@ -171,14 +159,14 @@ class PythonInterface:
             self.Hding_mag = XPLMGetDataf(self.drHding_mag)
             self.Mag_var = XPLMGetDataf(self.drMag_var)
             self.Alt_ind = XPLMGetDatad(self.drAlt_ind)
-            
+
             # get values for LXWP0
             self.IAS      = XPLMGetDataf(self.drIAS_ind)*1.852     # kias -> kph
             self.Baro_Alt = XPLMGetDataf(self.drBaro_alt)*0.3048   # feet-> meter  
             self.Vario    = XPLMGetDataf(self.drVario_fpm)*0.00508 # fpm -> m/s
             self.Wind_Dir = XPLMGetDataf(self.drWind_dir)
             self.Wind_Speed = XPLMGetDataf(self.drWind_speed)*1.852  # kias -> kph
-    
+
             # put in nmea format (matches x-plane | equipment | nmea serial feed)
             # time ssss.ssss since midnight --> hhmmss
             hh  = int(self.Zulu_time/3600)
@@ -187,7 +175,7 @@ class PythonInterface:
             n_time = str(hh).zfill(2)    \
                    + str(mm).zfill(2)    \
                    + str(ss).zfill(2) + '.00'
-    
+
             # lat and lon +/- ddd.dddd --> dddmm.mmm,h
             h = 'N'
             if self.Lat_deg < 0:
@@ -198,7 +186,7 @@ class PythonInterface:
             n_lat = str(ddd).zfill(2)        \
                           + ('%.4f' % mmm).zfill(7) + ','    \
                           + h
-    
+
             h = 'E'
             if self.Lon_deg < 0:
                 h = 'W'
@@ -208,14 +196,14 @@ class PythonInterface:
             n_lon = str(ddd).zfill(3)        \
                           + ('%.4f' % mmm).zfill(7) + ','    \
                           + h
-    
+
             # speed and heading may need some padding
             n_speed = ('%.1f' % self.Vgnd_kts).zfill(5)
             # heading mag --> true
             n_heading = '%.1f' % (self.Hding_mag - self.Mag_var)
-    
-            # date set once above 
-    
+
+            # date set once above
+
             # magnetic variation +/- dd.dddd --> ddd.d,h
             h = 'W'
             if self.Mag_var < 0:
@@ -224,10 +212,10 @@ class PythonInterface:
             ddd = '%.1f' % self.Mag_var
             n_magvar = str(ddd)    + ','    \
                      + h
-    
+
             # altitude meters
             n_alt = '%.1f' % self.Alt_ind
-    
+
             # construct the nmea gprmc sentence
             gprmc = 'GPRMC'        + ','    \
                   + n_time        + ','    \
@@ -238,27 +226,26 @@ class PythonInterface:
                   + n_heading    + ','    \
                   + self.n_date    + ','    \
                   + n_magvar
-    
+
             # append check sum and inital $
             cks = cksum(gprmc)
             gprmc = '$' + gprmc + '*' + cks + '\r\n'
-    
+
             # construct the nmea gpgga sentence
             gpgga = 'GPGGA'        + ','    \
                   + n_time        + ','    \
                   + n_lat        + ','    \
                   + n_lon        + ',1,04,0.0,'    \
                   + n_alt        + ',M,,,,'
-    
+
             # append check sum and inital $
             cks = cksum(gpgga)
             gpgga = '$' + gpgga + '*' + cks + '\r\n'
-    
+
             # pocketfms requires gpgsa sentence;
             # this one (constant) is what x-plane equipment setting sends.
             gpgsa = "$GPGSA,A,3,13,20,31,,,,,,,,,,02.2,02.2,*1e\r\n"
-      
-    # ----------------- 
+
             # alexferrer: add ias and pressure data so xcsoar can work the vario
             # construct the nmea lwxp0 sentence
             '''
@@ -280,7 +267,7 @@ class PythonInterface:
                                                             # airplane heading
             n_winddir = '%.1f' %self.Wind_Dir               # wind direction deg
             n_windspd = ('%.1f' % self.Wind_Speed).zfill(5) # kph
-                
+
             lxwp0 = 'LXWP0'    + ','    \
                   + n_logger   + ','    \
                   + n_ias      + ','    \
@@ -290,21 +277,15 @@ class PythonInterface:
                   + n_heading  + ','    \
                   + n_winddir  + ','    \
                   + n_windspd
-    
+
             # append check sum and inital $
             cks = cksum(lxwp0)
-            lxwp0 = '$' + lxwp0 + '*' + cks + '\r\n'  
-    # -------------------    
-    # -------------- test only ---------------------- 
+            lxwp0 = '$' + lxwp0 + '*' + cks + '\r\n'
             #print("NMEA> " ,  lxwp0)
-    
-    #        serial write at 4800 baud can take .3 sec, so put in own thread;
+
+            #serial write at 4800 baud can take .3 sec, so put in own thread;
             write_thread = threading.Thread(target=self.ser.write, args=(gprmc + gpgga + gpgsa + lxwp0,))
             write_thread.start()
-    
-    #        self.where = 5
-    #        self.OutputFile.write('where?  ' + str(self.where) + '  ' + str(time.clock()) + '\n')
-    #            self.LineCount = self.LineCount + 1
-    
+
             # Return s.s to indicate that we want to be called again in s.s seconds.
             return 1.0
